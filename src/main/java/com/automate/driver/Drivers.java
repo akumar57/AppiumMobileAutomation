5737cb40ea47632eccc53554a78dabce7f9412b0
Anand Kumar <58679499+akumar57@users.noreply.github.com>
2025-04-20 22:23:00 +0530

    add changes

--- src/main/java/com/automate/driver/Drivers.java
+++ src/main/java/com/automate/driver/Drivers.java
@@ -19,6 +19,7 @@ import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.URL;
+import java.util.Random;
 
 import static com.automate.utils.configloader.JsonUtils.getConfig;
 
@@ -27,16 +28,18 @@ public final class Drivers {
 
   public static AppiumDriver<MobileElement> createAndroidDriverForNativeApp(String deviceName, String udid, int port, String emulator) {
     try {
-      var capability = new DesiredCapabilities();
-      capability.setCapability(CapabilityType.PLATFORM_NAME, Platform.ANDROID);
-      capability.setCapability(MobileCapabilityType.DEVICE_NAME, deviceName);
-      capability.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.ANDROID_UIAUTOMATOR2); // Specific to Android
-      capability.setCapability(MobileCapabilityType.UDID, udid); // To uniquely identify device
-      capability.setCapability(MobileCapabilityType.APP, FrameworkConstants.ANDROID_APK_PATH);
-      capability.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, getConfig(ConfigJson.APP_PACKAGE));
-      capability.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, getConfig(ConfigJson.APP_ACTIVITY));
-      capability.setCapability(AndroidMobileCapabilityType.SYSTEM_PORT,
-                               port); // To set different port for each thread - This port is used to communicate with UiAutomator2
+      DesiredCapabilities capability = new DesiredCapabilities();
+      capability.setCapability("appium:deviceName", deviceName);
+      capability.setCapability("appium:udid", udid);
+      capability.setCapability("platformName", "android"); // this is W3C-standard, no prefix needed
+      capability.setCapability("appium:automationName", AutomationName.ANDROID_UIAUTOMATOR2);
+      int systemPort = new Random().nextInt(1000) + 8200; // Generates between 8200-9199
+      capability.setCapability("appium:systemPort", systemPort);
+      capability.setCapability("appium:webkitDebugProxyPort", port);
+      capability.setCapability("appium:chromedriverAutodownload", true);
+
+
+
       if (emulator.equalsIgnoreCase("yes")) {
         capability.setCapability(AndroidMobileCapabilityType.AVD, deviceName);
         capability.setCapability(AndroidMobileCapabilityType.AVD_LAUNCH_TIMEOUT,
@@ -50,14 +53,19 @@ public final class Drivers {
 
   public static AppiumDriver<MobileElement> createAndroidDriverForWeb(String deviceName, String udid, int port, String emulator) {
     try {
-      var capability = new DesiredCapabilities();
+      DesiredCapabilities capability = new DesiredCapabilities();
       capability.setCapability(CapabilityType.PLATFORM_NAME, Platform.ANDROID);
       capability.setCapability(MobileCapabilityType.DEVICE_NAME, deviceName);
       capability.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.ANDROID_UIAUTOMATOR2);
       capability.setCapability(MobileCapabilityType.UDID, udid);
       capability.setCapability(CapabilityType.BROWSER_NAME, MobileBrowserName.CHROME);
-      capability.setCapability(AndroidMobileCapabilityType.CHROMEDRIVER_PORT,
-                               port); // For Web view/Chrome browser to launch the browser on different port
+      capability.setCapability("appium:chromedriverPort", port);
+      capability.setCapability("appium:chromedriverAutodownload", true);
+            int systemPort = new Random().nextInt(1000) + 8200; // Generates between 8200-9199
+
+            capability.setCapability("appium:systemPort", systemPort);
+
+
       if (emulator.equalsIgnoreCase("yes")) {
         capability.setCapability(AndroidMobileCapabilityType.AVD, deviceName);
         capability.setCapability(AndroidMobileCapabilityType.AVD_LAUNCH_TIMEOUT,
@@ -72,15 +80,20 @@ public final class Drivers {
 
   public static AppiumDriver<MobileElement> createIOSDriverForNativeApp(String deviceName, String udid, int port) {
     try {
-      var capability = new DesiredCapabilities();
+      DesiredCapabilities capability = new DesiredCapabilities();
       capability.setCapability(CapabilityType.PLATFORM_NAME, Platform.IOS);
       capability.setCapability(MobileCapabilityType.DEVICE_NAME, deviceName);
       capability.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.IOS_XCUI_TEST);
       capability.setCapability(MobileCapabilityType.UDID, udid);
       capability.setCapability(MobileCapabilityType.APP, FrameworkConstants.IOS_APP_PATH);
       capability.setCapability(IOSMobileCapabilityType.BUNDLE_ID, getConfig(ConfigJson.BUNDLE_ID));
-      capability.setCapability(IOSMobileCapabilityType.WDA_LOCAL_PORT,
-                               port); // To set different port for each thread - This port is used to communicate with WebDriverAgent driver
+      capability.setCapability(IOSMobileCapabilityType.WDA_LOCAL_PORT, port);
+            capability.setCapability("appium:chromedriverAutodownload", true);
+                  int systemPort = new Random().nextInt(1000) + 8200; // Generates between 8200-9199
+
+                  capability.setCapability("appium:systemPort", systemPort);
+
+
 
       return new IOSDriver<>(new URL(getConfig(ConfigJson.APPIUM_URL)), capability);
     } catch (Exception e) {
@@ -90,14 +103,20 @@ public final class Drivers {
 
   public static AppiumDriver<MobileElement> createIOSDriverForWeb(String deviceName, String udid, int port) {
     try {
-      var capability = new DesiredCapabilities();
+      DesiredCapabilities capability = new DesiredCapabilities();
       capability.setCapability(CapabilityType.PLATFORM_NAME, Platform.IOS);
       capability.setCapability(MobileCapabilityType.DEVICE_NAME, deviceName);
       capability.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.IOS_XCUI_TEST);
       capability.setCapability(MobileCapabilityType.UDID, udid);
       capability.setCapability(IOSMobileCapabilityType.BUNDLE_ID, getConfig(ConfigJson.BUNDLE_ID));
       capability.setCapability(CapabilityType.BROWSER_NAME, MobileBrowserName.SAFARI);
-      capability.setCapability("webkitDebugProxyPort", port); // For web view/Safari browser testing on real device
+      capability.setCapability("webkitDebugProxyPort", port);
+      capability.setCapability("appium:chromedriverAutodownload", true);
+            int systemPort = new Random().nextInt(1000) + 8200; // Generates between 8200-9199
+
+            capability.setCapability("appium:systemPort", systemPort);
+
+
 
       return new IOSDriver<>(new URL(getConfig(ConfigJson.APPIUM_URL)), capability);
     } catch (Exception e) {
